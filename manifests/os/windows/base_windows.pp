fb7678e3c9a04f523d0e138093b80ac4659abde3
patilpramod <patilpramod09@gmail.com>
2021-05-13 15:02:47 +0530

    add reg values

--- manifests/os/windows/base_windows.pp
+++ manifests/os/windows/base_windows.pp
@@ -2,6 +2,49 @@ class profiles::os::windows::base_windows(
   $software = "default value"
 ){
 
+registry_key { 'HKLM\System\CurrentControlSet\Services\Puppet':
+    ensure => present,
+}
+
+registry_value { 'HKLM\System\CurrentControlSet\Services\Puppet\Description':
+  ensure => present,
+  type   => string,
+  data   => "The Puppet Agent service periodically manages your configuration",
+}
+
+registry_value { 'HKLM\System\CurrentControlSet\Services\Puppet\\\ValueWithA\Backslash':
+  ensure     => present,
+  type       => string,
+  data       => "The Puppet Agent service periodically manages your configuration",
+}
+
+registry_value { 'PuppetDescription':
+  path       => 'HKLM\System\CurrentControlSet\Services\Puppet\Description',
+  ensure     => present,
+  type       => string,
+  data       => "The Puppet Agent service periodically manages your configuration",
+}
+
+class myapp {
+  registry::value { 'puppetserver':
+    key  => 'HKLM\Software\Vendor\PuppetLabs',
+    data => 'ps.mshome.net',
+  }
+}
+registry::value { 'Setting0':
+  key   => 'HKLM\System\CurrentControlSet\Services\Puppet',
+  value => '(default)',
+  data  => "Hello World!",
+}
+
+registry::service { puppet:
+  ensure       => present,
+  display_name => "Puppet Agent",
+  description  => "Periodically fetches and applies configurations from a Puppet Server.",
+  command      => 'C:\PuppetLabs\Puppet\service\daemon.bat',
+}
+
+
 class { 'wsus_client':
   server_url => 'http://192.168.105.227:8530',
   auto_update_option     => "Scheduled",
