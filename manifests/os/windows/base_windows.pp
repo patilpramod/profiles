f51cc9e6e4162e39325a27e6eb6574acd251309b
patilpramod <patilpramod09@gmail.com>
2021-05-13 14:32:43 +0530

    calling wsus_client

--- manifests/os/windows/base_windows.pp
+++ manifests/os/windows/base_windows.pp
@@ -2,9 +2,14 @@ class profiles::os::windows::base_windows(
   $software = "default value"
 ){
 
-
-notify { 'Base Windows Profile test':
+class { 'wsus_client':
+  server_url => 'http://192.168.105.227:8530',
+  auto_update_option     => "Scheduled",
+  scheduled_install_day  => "Tuesday",
+  scheduled_install_hour => 2,
 }
+# notify { 'Base Windows Profile test':
+# }
 # class { 'os_patching':
 #     ensure => present,
 #   }
