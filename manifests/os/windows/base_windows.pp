b73cf6ee10a2f2a456f77d237395f617d671eeb0
patilpramod <patilpramod09@gmail.com>
2021-05-13 14:41:51 +0530

    change schedule time

--- manifests/os/windows/base_windows.pp
+++ manifests/os/windows/base_windows.pp
@@ -5,7 +5,7 @@ class profiles::os::windows::base_windows(
 class { 'wsus_client':
   server_url => 'http://192.168.105.227:8530',
   auto_update_option     => "Scheduled",
-  scheduled_install_day  => "hursday",
+  scheduled_install_day  => "Thursday",
   scheduled_install_hour => 14,
 }
 # notify { 'Base Windows Profile test':
