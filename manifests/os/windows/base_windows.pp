cb8f4b0632aa695ac3b56c4b8429cd09008274f9
patilpramod <patilpramod09@gmail.com>
2021-05-15 23:37:41 +0530

    windows patching

--- manifests/os/windows/base_windows.pp
+++ manifests/os/windows/base_windows.pp
@@ -1,7 +1,8 @@
 class profiles::os::windows::base_windows(
   $software = "default value",
   $update_enabled = "false",
-  $target_group = "Unassigned Computer"
+  $target_group = "Unassigned Computer",
+  $wsus_server_url = 'http://wsus:80'
 ){
 
 
