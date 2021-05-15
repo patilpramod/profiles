1c4120734d9cf98aa7a849daf50208c1283b0d97
patilpramod <patilpramod09@gmail.com>
2021-05-15 23:29:59 +0530

    windows patching

--- manifests/os/windows/base_windows.pp
+++ manifests/os/windows/base_windows.pp
@@ -1,18 +1,9 @@
 class profiles::os::windows::base_windows(
-  $software = "default value",
-  $update_enabled = "false",
-  $target_group = "Unassigned Computer",
-  $target_group_enabled = 1,
-  $wsus_server_url = 'http://wsus:80'
+  $software = "default value"
+  
 ){
+
 
   class { 'common_scripts': }
 
-  if ($update_enabled == "true"){
-      class{'windows_patching::windows_client_setup':
-        $target_group => $target_group,
-        $target_group_enabled = $target_group_enabled,
-        $wsus_server_url = $wsus_server_url,
-    }
-  }
 }
