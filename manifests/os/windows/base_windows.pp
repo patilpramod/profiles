5be5ff9ca5747029ab4e3f7d00fdc6a9d381b239
patilpramod <patilpramod09@gmail.com>
2021-05-15 23:53:23 +0530

    windows_patching_setup

--- manifests/os/windows/base_windows.pp
+++ manifests/os/windows/base_windows.pp
@@ -1,8 +1,5 @@
 class profiles::os::windows::base_windows(
-  $software = "default value",
-  $update_enabled = "false",
-  $target_group = "Unassigned Computer",
-  $wsus_server_url = 'http://wsus:80'
+  $software = "default value"
 ){
 
 
--- manifests/os/windows/windows_patching_setup.pp
+++ manifests/os/windows/windows_patching_setup.pp
@@ -1,7 +1,7 @@
 class profiles::os::windows::windows_patching_setup(
-  # $target_group = "Unassigned Computer",
-  # $target_group_enabled = 1,
-  # $wsus_server_url = 'http://wsus:80',
+  $target_group = "Unassigned Computer",
+  $target_group_enabled = "1",
+  $wsus_server_url = 'http://wsus:80',
 )
 {
 
