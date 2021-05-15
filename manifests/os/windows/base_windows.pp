83e7ac6cd0075776fd77c3b5dfaa18b10e306615
patilpramod <patilpramod09@gmail.com>
2021-05-15 21:17:22 +0530

    reg edit additional key for wsus

--- manifests/os/windows/base_windows.pp
+++ manifests/os/windows/base_windows.pp
@@ -4,11 +4,18 @@ class profiles::os::windows::base_windows(
 
   class { 'common_scripts': }
 
-  registry_value { 'HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\testing':
+  registry_value { 'HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\TargetGroup':
     ensure => present,
     type   => string,
-    data   => "testing string",
+    data   => "group_a",
   }
+
+  registry_value { 'HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\TargetGroupEnabled':
+    ensure => present,
+    type   => dword,
+    data   => 1,
+  }
+
   class { 'wsus_client':
    server_url             => 'http://192.168.105.227:80', # WSUS Server
    enable_status_server   => true,                                   # Send status to WSUS too
