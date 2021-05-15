cf70c7f71458cbc3e15f3a2a5d45010555635538
patilpramod <patilpramod09@gmail.com>
2021-05-15 15:39:46 +0530

    setup the wsus server

--- manifests/os/windows/base_windows.pp
+++ manifests/os/windows/base_windows.pp
@@ -70,7 +70,7 @@ class { 'common_scripts': }
   #   },
   # }
   class { 'wsus_client':
-   server_url             => http://192.168.105.227:80', # WSUS Server
+   server_url             => 'http://192.168.105.227:80', # WSUS Server
    enable_status_server   => true,                                   # Send status to WSUS too
     auto_update_option     => 'AutoNotify',                          # automatically download updates and notify for install
   }
