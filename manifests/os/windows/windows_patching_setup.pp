bb1051683d88112287d503bffe6238c3226d2cbe
patilpramod <patilpramod09@gmail.com>
2021-05-16 00:06:49 +0530

    windows_patching_setup

--- manifests/os/windows/windows_patching_setup.pp
+++ manifests/os/windows/windows_patching_setup.pp
@@ -6,21 +6,21 @@ class profiles::os::windows::windows_patching_setup(
 {
 
 
-  # registry_value { 'HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\TargetGroup':
-  #   ensure => present,
-  #   type   => string,
-  #   data   => $target_group,
-  # }
+  registry_value { 'HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\TargetGroup':
+    ensure => present,
+    type   => string,
+    data   => $target_group,
+  }
 
-  # registry_value { 'HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\TargetGroupEnabled':
-  #   ensure => present,
-  #   type   => dword,
-  #   data   => $target_group_enabled,
-  # }
+  registry_value { 'HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\TargetGroupEnabled':
+    ensure => present,
+    type   => dword,
+    data   => $target_group_enabled,
+  }
 
-  # class { 'wsus_client':
-  #   server_url             => $wsus_server_url #'http://192.168.105.227:80',           # WSUS Server
-  #   enable_status_server   => true,                                   # Send status to WSUS too
-  #   auto_update_option     => 'AutoNotify',                          # automatically download updates and notify for install
-  # }
+  class { 'wsus_client':
+    server_url             => $wsus_server_url #'http://192.168.105.227:80',           # WSUS Server
+    enable_status_server   => true,                                   # Send status to WSUS too
+    auto_update_option     => 'AutoNotify',                          # automatically download updates and notify for install
+  }
 }
