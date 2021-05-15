5569010f08a630b1535a76a1f1cf41b2b9aaed66
patilpramod <patilpramod09@gmail.com>
2021-05-15 21:20:38 +0530

    wsus params

--- manifests/os/windows/base_windows.pp
+++ manifests/os/windows/base_windows.pp
@@ -1,5 +1,8 @@
 class profiles::os::windows::base_windows(
-  $software = "default value"
+  $software = "default value",
+  $target_group = "Unassigned Computer",
+  $target_group_enabled = 1,
+  $wsus_server_url = 'http://wsus:80'
 ){
 
   class { 'common_scripts': }
@@ -7,17 +10,17 @@ class profiles::os::windows::base_windows(
   registry_value { 'HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\TargetGroup':
     ensure => present,
     type   => string,
-    data   => "group_a",
+    data   => $target_group,
   }
 
   registry_value { 'HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\TargetGroupEnabled':
     ensure => present,
     type   => dword,
-    data   => 1,
+    data   => $target_group_enabled,
   }
 
   class { 'wsus_client':
-   server_url             => 'http://192.168.105.227:80', # WSUS Server
+   server_url             => $wsus_server_url #'http://192.168.105.227:80',           # WSUS Server
    enable_status_server   => true,                                   # Send status to WSUS too
     auto_update_option     => 'AutoNotify',                          # automatically download updates and notify for install
   }
